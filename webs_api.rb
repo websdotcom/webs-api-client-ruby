84c375b4acd7268fb6de9e43c3c461cc997e7347
Patrick Carroll <patick@patrickomatic.com>
2010-12-29 22:17:49 -0500

    Added pagination, and don't have to supply the argument count with the WebsAPIRequest definitions

--- webs_api.rb
+++ webs_api.rb
@@ -7,10 +7,8 @@
 # @author Patrick Carroll <patrick@webs.com>
 #
 # TODO
-# * need to support pagination
 # * tests
 # * handle posting data (for example when installing an app)
-# * can the hash of request mappings be better expressed as a DSL?
 #
 require 'json'
 require 'net/https'
@@ -20,28 +18,24 @@ require 'uri'
 class WebsAPIRequest
 	API_URL = 'https://api.webs.com/'
 
-	attr_accessor :url, :arguments, :method, :requires_oauth_token
+	attr_accessor :url, :method, :requires_oauth_token
+
+	alias requires_oauth_token? requires_oauth_token
 
 
-	def initialize(url, arguments=0, method=:GET, requires_oauth_token=false)
+	def initialize(url, method=:GET, requires_oauth_token=false)
 		@url = url.start_with?(API_URL) ? url : API_URL + url
-		@arguments = arguments
 		@method = method
 		@requires_oauth_token = requires_oauth_token
 	end
 
-	def requires_oauth_token?
-		@requires_oauth_token
-	end
-
 	def make_url(args)
-		if arguments == 0
-			@url
-		elsif arguments == args.size
-			sprintf(@url, *args)
-		else
-			raise Exception, "URL #@url requires #@arguments arguments"
+		required_args = @url.count('%')
+		if args.size != required_args
+			raise Exception, "URL #@url requires #{required_args} arguments"
 		end
+
+		sprintf(@url, *args)
 	end
 
 	def to_s
@@ -53,42 +47,42 @@ end
 class WebsAPI
 	# the mapping of WebsAPIRequest objects to API calls
 	API_URLS = {
-		:get_apps => WebsAPIRequest.new('apps/'),
-		:get_app => WebsAPIRequest.new('apps/%s/', 1),
+		get_apps: WebsAPIRequest.new('apps/'),
+		get_app: WebsAPIRequest.new('apps/%s/'),
 
-		:get_templates => WebsAPIRequest.new('templates/'),
-		:get_template => WebsAPIRequest.new('templates/%s/', 1),
-		:get_template_styles => WebsAPIRequest.new('templates/%s/styles/', 1),
-		:get_template_style => WebsAPIRequest.new('templates/%s/styles/%s/', 2),
+		get_templates: WebsAPIRequest.new('templates/'),
+		get_template: WebsAPIRequest.new('templates/%s/'),
+		get_template_styles: WebsAPIRequest.new('templates/%s/styles/'),
+		get_template_style: WebsAPIRequest.new('templates/%s/styles/%s/'),
 
-		:get_site => WebsAPIRequest.new('sites/%s/', 1),
-		# :update_site => WebsAPIRequest.new('sites/%s/', 1, :PUT),
-		:get_site_feeds => WebsAPIRequest.new('sites/%s/feeds/', 1),
-		:get_site_navbar_links => WebsAPIRequest.new('sites/%s/navbar/', 1),
-		:get_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/%s/', 2),
-		:add_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/', 1, :POST, true),
-		:remove_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/%s', 2, :DELETE, true),
-		:get_site_sidebars => WebsAPIRequest.new('sites/%s/sidebars/', 1),
-		:get_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/', 2),
-		:add_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/', 2, :POST, true),
-		:remove_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/', 2, :DELETE, true),
+		get_site: WebsAPIRequest.new('sites/%s/'),
+		# update_site: WebsAPIRequest.new('sites/%s/', :PUT),
+		get_site_feeds: WebsAPIRequest.new('sites/%s/feeds/'),
+		get_site_navbar_links: WebsAPIRequest.new('sites/%s/navbar/'),
+		get_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/%s/'),
+		add_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/', :POST, true),
+		remove_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/%s', :DELETE, true),
+		get_site_sidebars: WebsAPIRequest.new('sites/%s/sidebars/'),
+		get_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/'),
+		add_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/', :POST, true),
+		remove_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/', :DELETE, true),
 
-		:get_installed_app => WebsAPIRequest.new('sites/%s/apps/%s/', 2),
-		:install_app => WebsAPIRequest.new('sites/%s/apps/%s/', 2, :POST, true),
-		:uninstall_app => WebsAPIRequest.new('sites/%s/apps/%s/', 2, :DELETE, true),
+		get_installed_app: WebsAPIRequest.new('sites/%s/apps/%s/'),
+		install_app: WebsAPIRequest.new('sites/%s/apps/%s/', :POST, true),
+		uninstall_app: WebsAPIRequest.new('sites/%s/apps/%s/', :DELETE, true),
 
-		:get_site_files => WebsAPIRequest.new('sites/%s/files/', 1),
-		:get_site_file => WebsAPIRequest.new('sites/%s/files/%s/', 2),
-		# :add_file_to_site => WebsAPIRequest.new('sites/%s/
-		# :update_file_on_site => 
+		get_site_files: WebsAPIRequest.new('sites/%s/files/'),
+		get_site_file: WebsAPIRequest.new('sites/%s/files/%s/'),
+		# add_file_to_site: WebsAPIRequest.new('sites/%s/
+		# update_file_on_site: 
 		
-		:get_site_members => WebsAPIRequest.new('sites/%s/members/', 1),
-		:get_site_member => WebsAPIRequest.new('sites/%s/members/%s/', 2),
-		:update_site_member => WebsAPIRequest.new('sites/%s/members/%s/', 2, :PUT, true),
-		:join_site_member => WebsAPIRequest.new('sites/%s/members/', 1, :POST, true),
-		:unsubscribe_site_member => WebsAPIRequest.new('sites/%s/members/%s/', 2, :DELETE, true),
-		:get_site_member_friends => WebsAPIRequest.new('sites/%s/members/%s/friends/', 2),
-		:get_site_member_feeds => WebsAPIRequest.new('sites/%s/members/%s/feeds/', 2),
+		get_site_members: WebsAPIRequest.new('sites/%s/members/'),
+		get_site_member: WebsAPIRequest.new('sites/%s/members/%s/'),
+		update_site_member: WebsAPIRequest.new('sites/%s/members/%s/', :PUT, true),
+		join_site_member: WebsAPIRequest.new('sites/%s/members/', :POST, true),
+		unsubscribe_site_member: WebsAPIRequest.new('sites/%s/members/%s/', :DELETE, true),
+		get_site_member_friends: WebsAPIRequest.new('sites/%s/members/%s/friends/'),
+		get_site_member_feeds: WebsAPIRequest.new('sites/%s/members/%s/feeds/'),
 	}
 
 	attr_accessor :oauth_token
@@ -100,11 +94,11 @@ class WebsAPI
 
 
 	def respond_to?(sym)
-		super or API_URLS.has_key?(sym)
+		super or API_URLS.has_key? sym
 	end
 
 	def method_missing(sym, *args, &block)
-		unless API_URLS.has_key?(sym)
+		unless API_URLS.has_key? sym
 			raise NoMethodError, 
 				"undefined method '#{sym}' for #{inspect}:#{self.class}"
 		end
@@ -115,30 +109,43 @@ class WebsAPI
 				"#{api_url} requires that an OAuth token is set"
 		end
 
-		url = api_url.make_url(args)
-		url << "?oauth_token=#{@oauth_token}" if @oauth_token
+		page_num, page_size = nil, nil
+		if args.last.is_a? Hash
+			hash_args = args.pop
+			page_num, page_size = hash_args[:page_num], hash_args[:page_size]
+		end
 
-		JSON.parse(http_request(url, api_url.method))
+
+		url = api_url.make_url(args) 
+
+		query = []
+		query << "oauth_token=#{@oauth_token}" if @oauth_token
+		query << "page=#{page_num}" if page_num
+		query << "pagesize=#{page_size}" if page_size
+
+		url << "?" + query.join("&") if not query.empty?
+
+		JSON.parse http_request(url, api_url.method)
 	end
 
 
 	protected
 
 	def http_request(url, method)
-		uri = URI.parse(url)
+		uri = URI.parse url
 		http = Net::HTTP.new(uri.host, uri.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 		request = case method
 				  when :GET 
-					  Net::HTTP::Get.new(uri.request_uri)
+					  Net::HTTP::Get.new uri.request_uri
 				  when :PUT
-					  Net::HTTP::Put.new(uri.request_uri)
+					  Net::HTTP::Put.new uri.request_uri
 				  when :POST
-					  Net::HTTP::Post.new(uri.request_uri)
+					  Net::HTTP::Post.new uri.request_uri
 				  when :DELETE
-					  Net::HTTP::Delete.new(uri.request_uri)
+					  Net::HTTP::Delete.new uri.request_uri
 				  else
 					  raise Exception, 
 						  "Unknown request method: #{method.to_s} for #{url}"
@@ -149,3 +156,5 @@ class WebsAPI
 	end
 end
 
+
+WebsAPI.new.get_site('patrick', page_num: 1, page_size: 20)
