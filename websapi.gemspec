885330760ac390971f313968c191f49460df623a
Patrick Carroll <patick@patrickomatic.com>
2011-01-10 10:45:41 -0500

    Renaming files to not use underscores

---
+++ websapi.gemspec
@@ -0,0 +1,23 @@
+Gem::Specification.new do |s|
+	s.name = 'websapi'
+	s.rubyforge_project = 'websapi'
+	s.version = '0.1.0'
+	s.platform = Gem::Platform::RUBY
+	s.required_rubygems_version = Gem::Requirement.new('>= 1.2') if s.respond_to? :required_rubygems_version=
+	s.authors = ['Patrick Carroll']
+	s.email = 'patrick@webs.com'
+	s.date = '2011-01-04'
+	s.description = 'Client for the Webs.com REST API.'
+	s.files = `git ls-files`.split("\n")
+	s.homepage = 'http://github.com/websdotcom/webs-api-client-ruby'
+	s.summary = 'Client for the Webs.com REST API.'
+
+	s.add_dependency('json')
+
+	s.add_development_dependency('rspec')
+
+	if s.respond_to? :specification_version then
+		current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+		s.specification_version = 3
+	end
+end
--- websapi.rb
+++ websapi.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby1.9.1
+#!/usr/bin/env ruby
 # 
 # API client for the Webs.com REST API.  
 #
@@ -51,42 +51,42 @@ end
 class WebsAPI
 	# the mapping of WebsAPIRequest objects to API calls
 	API_URLS = {
-		get_apps: WebsAPIRequest.new('apps/'),
-		get_app: WebsAPIRequest.new('apps/%s/'),
+		:get_apps => WebsAPIRequest.new('apps/'),
+		:get_app => WebsAPIRequest.new('apps/%s/'),
 
-		get_templates: WebsAPIRequest.new('templates/'),
-		get_template: WebsAPIRequest.new('templates/%s/'),
-		get_template_styles: WebsAPIRequest.new('templates/%s/styles/'),
-		get_template_style: WebsAPIRequest.new('templates/%s/styles/%s/'),
+		:get_templates => WebsAPIRequest.new('templates/'),
+		:get_template => WebsAPIRequest.new('templates/%s/'),
+		:get_template_styles => WebsAPIRequest.new('templates/%s/styles/'),
+		:get_template_style => WebsAPIRequest.new('templates/%s/styles/%s/'),
 
-		get_site: WebsAPIRequest.new('sites/%s/'),
-		# update_site: WebsAPIRequest.new('sites/%s/', :PUT),
-		get_site_feeds: WebsAPIRequest.new('sites/%s/feeds/'),
-		get_site_navbar_links: WebsAPIRequest.new('sites/%s/navbar/'),
-		get_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/%s/'),
-		add_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/', :POST, true),
-		remove_site_navbar_link: WebsAPIRequest.new('sites/%s/navbar/%s', :DELETE, true),
-		get_site_sidebars: WebsAPIRequest.new('sites/%s/sidebars/'),
-		get_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/'),
-		add_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/', :POST, true),
-		remove_site_sidebar: WebsAPIRequest.new('sites/%s/sidebars/%s/', :DELETE, true),
+		:get_site => WebsAPIRequest.new('sites/%s/'),
+		# :update_site => WebsAPIRequest.new('sites/%s/', :PUT),
+		:get_site_feeds => WebsAPIRequest.new('sites/%s/feeds/'),
+		:get_site_navbar_links => WebsAPIRequest.new('sites/%s/navbar/'),
+		:get_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/%s/'),
+		:add_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/', :POST, true),
+		:remove_site_navbar_link => WebsAPIRequest.new('sites/%s/navbar/%s/', :DELETE, true),
+		:get_site_sidebars => WebsAPIRequest.new('sites/%s/sidebars/'),
+		:get_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/'),
+		:add_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/', :POST, true),
+		:remove_site_sidebar => WebsAPIRequest.new('sites/%s/sidebars/%s/', :DELETE, true),
 
-		get_installed_app: WebsAPIRequest.new('sites/%s/apps/%s/'),
-		install_app: WebsAPIRequest.new('sites/%s/apps/%s/', :POST, true),
-		uninstall_app: WebsAPIRequest.new('sites/%s/apps/%s/', :DELETE, true),
+		:get_installed_app => WebsAPIRequest.new('sites/%s/apps/%s/'),
+		:install_app => WebsAPIRequest.new('sites/%s/apps/%s/', :POST, true),
+		:uninstall_app => WebsAPIRequest.new('sites/%s/apps/%s/', :DELETE, true),
 
-		get_site_files: WebsAPIRequest.new('sites/%s/files/'),
-		get_site_file: WebsAPIRequest.new('sites/%s/files/%s/'),
-		# add_file_to_site: WebsAPIRequest.new('sites/%s/
-		# update_file_on_site: 
+		:get_site_files => WebsAPIRequest.new('sites/%s/files/'),
+		:get_site_file => WebsAPIRequest.new('sites/%s/files/%s/'),
+		# :add_file_to_site => WebsAPIRequest.new('sites/%s/
+		# :update_file_on_site =>
 		
-		get_site_members: WebsAPIRequest.new('sites/%s/members/'),
-		get_site_member: WebsAPIRequest.new('sites/%s/members/%s/'),
-		update_site_member: WebsAPIRequest.new('sites/%s/members/%s/', :PUT, true),
-		join_site_member: WebsAPIRequest.new('sites/%s/members/', :POST, true),
-		unsubscribe_site_member: WebsAPIRequest.new('sites/%s/members/%s/', :DELETE, true),
-		get_site_member_friends: WebsAPIRequest.new('sites/%s/members/%s/friends/'),
-		get_site_member_feeds: WebsAPIRequest.new('sites/%s/members/%s/feeds/'),
+		:get_site_members => WebsAPIRequest.new('sites/%s/members/'),
+		:get_site_member => WebsAPIRequest.new('sites/%s/members/%s/'),
+		:update_site_member => WebsAPIRequest.new('sites/%s/members/%s/', :PUT, true),
+		:join_site_member => WebsAPIRequest.new('sites/%s/members/', :POST, true),
+		:unsubscribe_site_member => WebsAPIRequest.new('sites/%s/members/%s/', :DELETE, true),
+		:get_site_member_friends => WebsAPIRequest.new('sites/%s/members/%s/friends/'),
+		:get_site_member_feeds => WebsAPIRequest.new('sites/%s/members/%s/feeds/'),
 	}
 
 	attr_accessor :oauth_token
